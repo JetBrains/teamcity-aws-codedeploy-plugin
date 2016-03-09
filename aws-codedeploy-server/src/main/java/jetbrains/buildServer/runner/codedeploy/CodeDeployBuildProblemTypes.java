c996a521fdd91f58b85296dc1b5c7a9a3929e398
Victory Petrenko <victory.bedrosova@jetbrains.com>
2016-03-09 16:38:18 +0100

    provided human-readable build problem type descriptions for CodeDeploy build problem types

---
+++ aws-codedeploy-server/src/main/java/jetbrains/buildServer/runner/codedeploy/CodeDeployBuildProblemTypes.java
@@ -0,0 +1,38 @@
+package jetbrains.buildServer.runner.codedeploy;
+
+import jetbrains.buildServer.ExtensionHolder;
+import jetbrains.buildServer.serverSide.problems.BaseBuildProblemTypeDetailsProvider;
+import jetbrains.buildServer.serverSide.problems.BuildProblemTypeDetailsProvider;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author vbedrosova
+ */
+public class CodeDeployBuildProblemTypes {
+
+  public CodeDeployBuildProblemTypes(@NotNull ExtensionHolder extensionHolder) {
+    register(CodeDeployConstants.TIMEOUT_BUILD_PROBLEM_TYPE, "Deployment timeout", extensionHolder);
+    register(CodeDeployConstants.FAILURE_BUILD_PROBLEM_TYPE, "Deployment failure", extensionHolder);
+    register(CodeDeployConstants.EXCEPTION_BUILD_PROBLEM_TYPE, "Deployment unexpected failure", extensionHolder);
+    register(CodeDeployConstants.SERVICE_PROBLEM_TYPE, "AWS service exception", extensionHolder);
+    register(CodeDeployConstants.CLIENT_PROBLEM_TYPE, "AWS client exception", extensionHolder);
+  }
+
+  private static void register(@NotNull final String type, @NotNull final String descr, @NotNull final ExtensionHolder extensionHolder) {
+    extensionHolder.registerExtension(BuildProblemTypeDetailsProvider.class,
+      type,
+      new BaseBuildProblemTypeDetailsProvider() {
+        @NotNull
+        public String getType() {
+          return type;
+        }
+
+        @NotNull
+        @Override
+        public String getTypeDescription() {
+          return descr;
+        }
+      });
+  }
+
+}
