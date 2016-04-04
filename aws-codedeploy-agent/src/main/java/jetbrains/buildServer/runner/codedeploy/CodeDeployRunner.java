7acd2919e946f5fd9c2afcaf8e50ecd84b78acca
Victory Petrenko <victory.bedrosova@jetbrains.com>
2016-04-04 10:12:51 +0200

    TW-44766, TW-44797

--- aws-codedeploy-agent/src/main/java/jetbrains/buildServer/runner/codedeploy/CodeDeployRunner.java
+++ aws-codedeploy-agent/src/main/java/jetbrains/buildServer/runner/codedeploy/CodeDeployRunner.java
@@ -60,7 +60,7 @@ public class CodeDeployRunner implements AgentBuildRunner {
         final String s3BucketName = runnerParameters.get(CodeDeployConstants.S3_BUCKET_NAME_PARAM);
         String s3ObjectKey = runnerParameters.get(CodeDeployConstants.S3_OBJECT_KEY_PARAM);
 
-        if (isUploadStepEnabled) {
+        if (isUploadStepEnabled && !problemOccurred.get() && !isInterrupted()) {
           final File readyRevision = new ApplicationRevision(
             StringUtil.isEmptyOrSpaces(s3ObjectKey) ? runningBuild.getBuildTypeExternalId() : s3ObjectKey,
             runnerParameters.get(CodeDeployConstants.REVISION_PATHS_PARAM),
@@ -78,11 +78,11 @@ public class CodeDeployRunner implements AgentBuildRunner {
         final String bundleType = "" + AWSClient.getBundleType(s3ObjectKey);
         final String s3ObjectVersion = StringUtil.nullIfEmpty(configParameters.get(CodeDeployConstants.S3_OBJECT_VERSION_CONFIG_PARAM));
 
-        if (isRegisterStepEnabled) {
+        if (isRegisterStepEnabled && !problemOccurred.get() && !isInterrupted()) {
           awsClient.registerRevision(s3BucketName, s3ObjectKey, bundleType, s3ObjectVersion, applicationName);
         }
 
-        if (isDeployStepEnabled) {
+        if (isDeployStepEnabled && !problemOccurred.get() && !isInterrupted()) {
           final String deploymentGroupName = runnerParameters.get(CodeDeployConstants.DEPLOYMENT_GROUP_NAME_PARAM);
           final String deploymentConfigName = StringUtil.nullIfEmpty(runnerParameters.get(CodeDeployConstants.DEPLOYMENT_CONFIG_NAME_PARAM));
 
