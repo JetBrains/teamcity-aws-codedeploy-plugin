0df5ee707af14c102ca4d6e433d1bccce96b54c6
Victory Petrenko <victory.bedrosova@jetbrains.com>
2016-08-29 13:13:04 +0200

    test for TW-46802

--- aws-codedeploy-common/src/test/java/jetbrains/buildServer/runner/codedeploy/ParametersValidatorTest.java
+++ aws-codedeploy-common/src/test/java/jetbrains/buildServer/runner/codedeploy/ParametersValidatorTest.java
@@ -84,6 +84,21 @@ public class ParametersValidatorTest extends BaseTestCase {
   }
 
   @Test
+  public void deploy_params_with_references() {
+    then(validate(
+      DEPLOYMENT_STEPS_PARAM, DEPLOY_STEP,
+      REGION_NAME_PARAM, "us-east-1",
+      CREDENTIALS_TYPE_PARAM, ACCESS_KEYS_OPTION,
+      ACCESS_KEY_ID_PARAM, "%access.key.id%",
+      SECURE_SECRET_ACCESS_KEY_PARAM, "%secret.access.key.id%",
+      S3_BUCKET_NAME_PARAM, "%s3.bucket.name%",
+      S3_OBJECT_KEY_PARAM, "%s3.object.key%",
+      APP_NAME_PARAM, "%app.name%",
+      DEPLOYMENT_GROUP_NAME_PARAM, "%deployment.group%"
+    )).as("Must respect param refs").isEmpty();
+  }
+
+  @Test
   public void unexpected_revision_paths() {
     then(validate(DEPLOYMENT_STEPS_PARAM, UPLOAD_STEP, REVISION_PATHS_PARAM, "=>")).as("Must detect unexpected revision paths").
       containsEntry(REVISION_PATHS_PARAM, "Application revision has unexpected value, Ant-style wildcards as well as target directories like out/**/*.zip => dist supported");
