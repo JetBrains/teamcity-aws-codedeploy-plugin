523f52ebfa6905f8790ab9e75266f44e8417203d
Sergey Pak <paksv@yandex.ru>
2018-05-17 17:07:31 +0200

    updated HR names for AWS regions

--- amazon-util/src/main/java/jetbrains/buildServer/util/amazon/AWSRegions.java
+++ amazon-util/src/main/java/jetbrains/buildServer/util/amazon/AWSRegions.java
@@ -38,14 +38,18 @@ public final class AWSRegions {
     REGION_NAMES_FOR_WEB.put("us-west-2", "US West (Oregon)");
     REGION_NAMES_FOR_WEB.put("eu-west-1", "EU West (Dublin)");
     REGION_NAMES_FOR_WEB.put("eu-west-2", "EU West (London)");
+    REGION_NAMES_FOR_WEB.put("eu-west-3", "EU West (Paris)");
+    REGION_NAMES_FOR_WEB.put("ca-central-1", "Canada (Central)");
     REGION_NAMES_FOR_WEB.put("eu-central-1", "EU Central (Frankfurt)");
     REGION_NAMES_FOR_WEB.put("sa-east-1", "South America (Sao Paulo)");
+    REGION_NAMES_FOR_WEB.put("ap-south-1", "Asia Pacific (Mumbai)");
     REGION_NAMES_FOR_WEB.put("ap-northeast-1", "Asia Pacific (Tokyo)");
     REGION_NAMES_FOR_WEB.put("ap-northeast-2", "Asia Pacific (Seoul)");
     REGION_NAMES_FOR_WEB.put("ap-southeast-1", "Asia Pacific (Singapore)");
     REGION_NAMES_FOR_WEB.put("ap-southeast-2", "Asia Pacific (Sydney)");
     REGION_NAMES_FOR_WEB.put("us-gov-west-1", "AWS GovCloud (US)");
     REGION_NAMES_FOR_WEB.put("cn-north-1", "China (Beijing)");
+    REGION_NAMES_FOR_WEB.put("cn-northwest-1", "China (Ningxia)");
 
     for (Regions region : Regions.values()) {
       if (REGION_NAMES_FOR_WEB.containsKey(region.getName())) continue;
