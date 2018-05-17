60da08e182108c039bbe885b08b5369b3a7bffa6
Sergey Pak <paksv@yandex.ru>
2018-05-17 17:55:34 +0200

    proper ordering for regions

--- amazon-util/src/main/java/jetbrains/buildServer/util/amazon/AWSRegions.java
+++ amazon-util/src/main/java/jetbrains/buildServer/util/amazon/AWSRegions.java
@@ -36,10 +36,10 @@ public final class AWSRegions {
     REGION_NAMES_FOR_WEB.put("us-east-2", "US East (Ohio)");
     REGION_NAMES_FOR_WEB.put("us-west-1", "US West (N. California)");
     REGION_NAMES_FOR_WEB.put("us-west-2", "US West (Oregon)");
+    REGION_NAMES_FOR_WEB.put("ca-central-1", "Canada (Central)");
     REGION_NAMES_FOR_WEB.put("eu-west-1", "EU West (Dublin)");
     REGION_NAMES_FOR_WEB.put("eu-west-2", "EU West (London)");
     REGION_NAMES_FOR_WEB.put("eu-west-3", "EU West (Paris)");
-    REGION_NAMES_FOR_WEB.put("ca-central-1", "Canada (Central)");
     REGION_NAMES_FOR_WEB.put("eu-central-1", "EU Central (Frankfurt)");
     REGION_NAMES_FOR_WEB.put("sa-east-1", "South America (Sao Paulo)");
     REGION_NAMES_FOR_WEB.put("ap-south-1", "Asia Pacific (Mumbai)");
